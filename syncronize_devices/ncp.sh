d5d7a3345a85aba2b36ceb69966f287538efa01a
Khai Duong <duongkhai@gmail.com>
2022-11-21 23:59:07 +0100

    Add add main function and setup template variables

--- syncronize_devices/ncp.sh
+++ syncronize_devices/ncp.sh
@@ -1,34 +1,36 @@
 #!/usr/bin/bash
 
 # Assign your ip addresses underneath
-DEVICE1=''                                  # DEVICE1 ip address
-DEVICE2=''                                  # DEVICE2 ip address
+DEVICE1='' # DEVICE1 ip address
+DEVICE2='' # DEVICE2 ip address
 
-THIS=''                                     # This computer's ip address
-TARGET=''                                   # Target computer's ip address
+THIS=''   # This computer's ip address
+TARGET='' # Target computer's ip address
 
 # Assign your username here
-USER=''                                     # Host username
-NAME="ncp"                                  # Script name
-PING_COUNT=1                                # How many times to ping the target
+USER="$(whoami)" # Host username
+NAME="ncp"       # Script name
+PING_COUNT=1     # How many times to ping the target
 
 # Assign directories to sync here. Note that you need read/write permissions
-DIRS=(                                      # Directories to sync
+DIRS=(# Directories to sync
     "Documents"
     "Downloads"
 )
 
 ####
-# Updates self and target ip
+# Download from target device
 ####
-function self_ip() {
-    # Set this computer's IP address
-    IPS='\n' read -ra THIS <<< "$(ip route | awk '{print $9}')" 
-    case "${THIS[0]}" in
-        "$DEVICE2" ) TARGET="$DEVICE1" ;;
-        "$DEVICE1" ) TARGET="$DEVICE2" ;;
-    esac
-    return 0
+function download() {
+    if pingTarget; then
+        echo "Downloading..."
+        for dir in "${DIRS[@]}"; do
+            rsync -auv "$USER@$TARGET:/home/$USER/$dir" "$HOME" # Download content from remote to local machine via SSH
+        done
+    else
+        echo "Could not reach target"
+        return 1
+    fi
 }
 
 ####
@@ -37,14 +39,45 @@ function self_ip() {
 function pingTarget() {
     # Pings the target
     printf "Finding target address %s...\n" "$TARGET"
-    if ! ping "$TARGET" -c "$PING_COUNT" 1> /dev/null; then
+    if ! ping "$TARGET" -c "$PING_COUNT" 1>/dev/null; then
         printf "Could not find target address %s\n" "$TARGET"
         return 1
     else
         return 0
     fi
 }
-`
+
+####
+# Updates self and target ip
+####
+function self_ip() {
+    # Set this computer's IP address
+    IPS='\n' read -ra THIS <<<"$(ip route | awk '{print $9}')"
+    case "${THIS[0]}" in
+    "$DEVICE2") TARGET="$DEVICE1" ;;
+    "$DEVICE1") TARGET="$DEVICE2" ;;
+    esac
+    return 0
+}
+
+####
+# Upload
+####
+function upload() {
+    if pingTarget; then
+        echo "Uploading..."
+        for dir in "${DIRS[@]}"; do
+            rsync -auv "$HOME/$dir" "$USER@$TARGET:/home/$USER" # Upload content from local to remote machine via SSH
+        done
+    else
+        echo "Could not reach target"
+        return 1
+    fi
+}
+
+####
+# Help page
+####
 function usage() {
     echo -n "Usage: $NAME [OPTION] ...
 
@@ -57,38 +90,21 @@ Options:
 "
 }
 
-# Check password correctness
-
-if [ -z "$1" ]; then
-    usage
-    exit 0
-fi
-
-self_ip
-
-case "$1" in
-"-d" | "--download")
-    if pingTarget; then
-        echo "Downloading..."
-        for dir in "${DIRS[@]}"; do
-            rsync -auv "$USER@$TARGET:/home/$USER/$dir" "$HOME" # Download content from remote to local machine via SSH
-        done
-    else
-        echo "Could not reach target"
-        exit 1
+function main() {
+    if [ -z "$1" ]; then
+        usage
+        return 0
     fi
-    ;;
-"-u" | "--upload")
-    if pingTarget; then
-        echo "Uploading..."
-        for dir in "${DIRS[@]}"; do
-            rsync -auv "$HOME/$dir" "$USER@$TARGET:/home/$USER" # Upload content from local to remote machine via SSH
-        done
-    else
-        echo "Could not reach target"
-        exit 1
-    fi
-    ;;
-*)
-    usage
-esac
+
+    # Figures out this device's ip address
+    self_ip
+
+    case "$1" in
+    "-d" | "--download") download ;;
+    "-u" | "--upload") upload ;;
+    *) usage ;;
+    esac
+}
+
+main "$@"
+exit "$?"
