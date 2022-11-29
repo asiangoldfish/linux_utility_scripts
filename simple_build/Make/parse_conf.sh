e0eef820326de6ceb802f791940102a56f58644b
asiangoldfish <duongkhai@gmail.com>
2022-11-29 20:36:54 +0100

    Implement Make clean functionality

--- simple_build/Make/parse_conf.sh
+++ simple_build/Make/parse_conf.sh
@@ -2,6 +2,8 @@
 BUILD_FILE="Makefile"     # Special configuration file in project dir
 BUILD_COMMAND="make"          # The build system's invocation command
 
+CLEAN_COMMAND="make clean"
+
 # Parse Make options
 function parse_options() {
     # Source the options file
