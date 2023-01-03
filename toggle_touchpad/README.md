1943a7811bb314386b86cf761e37eb222c8b337d
asiangoldfish <duongkhai@gmail.com>
2023-01-03 21:52:34 +0100

    Support persistent save of touchpad state through reboot/restarting GUI session

--- toggle_touchpad/README.md
+++ toggle_touchpad/README.md
@@ -1,2 +1,5 @@
 # Toggle Touchpad
 Execute `toggle_touchpad.sh` to deactivate or activate your device's touchpad, if it supports it.
+
+# How it works
+The script depends on the file $HOME/.touchpad that stores either 0 or 1. This tells the script at what state the touchpad should be at, allowing the script to persist after logging off the GUI session.
--- toggle_touchpad/toggle_touchpad.sh
+++ toggle_touchpad/toggle_touchpad.sh
@@ -1,23 +1,44 @@
 #!/usr/bin/bash
 
-# Iterate all lines to find the Touchpad, if the device is supported
-if ! xinput list | grep -q "Touchpad"; then
-    echo "Touchpad is not supported"
-    exit 1
-fi
+function main() {
 
-# Find the device id. Call it by using the following syntax:
-# ${found_id[1]}
-IFS='=' read -ra found_id <<< "$( xinput list | grep "Touchpad" | awk '{print $6}' )"
-id="${found_id[1]}"
+    STATE_FILE="$HOME/.touchpad_toggle"     # File to store state in
+    STATE="$( cat "$STATE_FILE" || echo 1 )"          # Touchpad ON/OFF state
+    
+    # Iterate all lines to find the Touchpad, if the device is supported
+    if ! xinput list | grep -q "Touchpad"; then
+        echo "Touchpad is not supported"
+        exit 1
+    fi
 
-# Check if device is enabled. The value is inverted, so invert before using it
-isDeviceEnabled="$( xinput list-props 11 | grep "Device Enabled" | awk '{print $4}' )"
+    # Find the device id. Call it by using the following syntax:
+    # ${found_id[1]}
+    IFS='=' read -ra found_id <<< "$( xinput list | grep "Touchpad" | awk '{print $6}' )"
+    id="${found_id[1]}"
 
-if [[ "$isDeviceEnabled" -eq 0 ]]; then
-    xinput set-prop "$id" "Device Enabled" 1
-    echo "Touchpad enabled"
-else
-    xinput set-prop "$id" "Device Enabled" 0
-    echo "Touchpad disabled"
-fi
+    # Check if device is enabled. The value is inverted, so invert before using it
+    #isDeviceEnabled="$( xinput list-props 11 | grep "Device Enabled" | awk '{print $4}' )"
+
+    if [ -z "$RESTORE" ]; then
+        if [ "$STATE" == 0 ]; then
+            STATE=1
+            echo "Touchpad enabled"
+        else
+            STATE=0
+            echo "Touchpad disabled"
+        fi
+
+        # Store the new state in STATE_FILE
+        printf '%s' "$STATE" > "$STATE_FILE"
+    fi
+
+    # Toggle touchpad
+    xinput set-prop "$id" "Device Enabled" "$STATE"
+}
+
+case "$1" in 
+    "--restore" )
+        RESTORE=1
+esac
+
+main
